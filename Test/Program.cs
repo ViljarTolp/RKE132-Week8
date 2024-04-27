bbb3f27f5eac06b2ac5dd6ea4362ac5241c77aa9
ViljarT <viljar.tolp@tktk.ee>
2024-04-27 17:25:14 +0300

    Valma

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿string path = @"L:\My Drive\Programmeerimise algkursus\Tund 08";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] fileContentArray)
+{
+    int i = 1;
+
+    foreach (string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {line}");
+        i++;
+    }
+}
